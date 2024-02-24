419f71de44c60e6da26b17a44056bbeba4177699
dhkatrv76 <dave.hongk@gmail.com>
2024-02-24 09:43:58 -0800

    scrollable for rotations

--- lib/presentation/pages/login_page.dart
+++ lib/presentation/pages/login_page.dart
@@ -14,91 +14,93 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const SizedBox(height: 150),
+        child: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(height: 150),
 
-              // Logo or Sign in message
-              const Text(
-                'Sign In',
-                style: TextStyle(fontSize: 32),
-              ),
-
-              const SizedBox(height: 25),
-
-              Text(
-                'Welcome back!',
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.grey[700],
+                // Logo or Sign in message
+                const Text(
+                  'Sign In',
+                  style: TextStyle(fontSize: 32),
                 ),
-              ),
 
-              const SizedBox(height: 25),
+                const SizedBox(height: 25),
 
-              // username or phone textfield
-              InputTextField(
-                controller: _emailController,
-                hintText: 'Email or Phone Number',
-              ),
+                Text(
+                  'Welcome back!',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.grey[700],
+                  ),
+                ),
 
-              const SizedBox(height: 20),
+                const SizedBox(height: 25),
 
-              // password input
-              PasswordTextField(
-                controller: _passwordController,
-                hintText: 'Password',
-              ),
+                // username or phone textfield
+                InputTextField(
+                  controller: _emailController,
+                  hintText: 'Email or Phone Number',
+                ),
 
-              const SizedBox(height: 10),
+                const SizedBox(height: 20),
 
-              // Forgot password link
-              Padding(
-                padding: const EdgeInsets.only(right: 25.0),
-                child: Align(
-                  alignment: Alignment.centerRight,
-                  child: RichText(
-                    text: TextSpan(
-                      text: 'Forgot Password?',
-                      style: const TextStyle(color: Colors.orange),
-                      recognizer: TapGestureRecognizer()..onTap = () {},
+                // password input
+                PasswordTextField(
+                  controller: _passwordController,
+                  hintText: 'Password',
+                ),
+
+                const SizedBox(height: 10),
+
+                // Forgot password link
+                Padding(
+                  padding: const EdgeInsets.only(right: 25.0),
+                  child: Align(
+                    alignment: Alignment.centerRight,
+                    child: RichText(
+                      text: TextSpan(
+                        text: 'Forgot Password?',
+                        style: const TextStyle(color: Colors.orange),
+                        recognizer: TapGestureRecognizer()..onTap = () {},
+                      ),
                     ),
                   ),
                 ),
-              ),
 
-              const SizedBox(height: 50),
+                const SizedBox(height: 50),
 
-              // Button
-              const SubmitButton(buttonText: 'Sign In'),
+                // Button
+                const SubmitButton(buttonText: 'Sign In'),
 
-              const SizedBox(height: 20),
+                const SizedBox(height: 20),
 
-              // Sign-up link
-              Padding(
-                padding: const EdgeInsets.only(left: 25.0),
-                child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: RichText(
-                    text: TextSpan(
-                      children: [
-                        TextSpan(
-                          text: 'Don\'t have an account? ',
-                          style: TextStyle(color: Colors.grey.shade700),
-                        ),
-                        TextSpan(
-                          text: 'Sign Up',
-                          style: const TextStyle(color: Colors.orange),
-                          recognizer: TapGestureRecognizer()..onTap = () {},
-                        ),
-                      ],
+                // Sign-up link
+                Padding(
+                  padding: const EdgeInsets.only(left: 25.0),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: RichText(
+                      text: TextSpan(
+                        children: [
+                          TextSpan(
+                            text: 'Don\'t have an account? ',
+                            style: TextStyle(color: Colors.grey.shade700),
+                          ),
+                          TextSpan(
+                            text: 'Sign Up',
+                            style: const TextStyle(color: Colors.orange),
+                            recognizer: TapGestureRecognizer()..onTap = () {},
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
