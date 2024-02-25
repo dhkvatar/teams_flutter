10e7cd733839efe3e8c31aeee115e68f2ac08296
dhkatrv76 <dave.hongk@gmail.com>
2024-02-25 10:19:58 -0800

    wrap columns with singlechildscrollview

--- lib/presentation/pages/sign_up_page.dart
+++ lib/presentation/pages/sign_up_page.dart
@@ -10,78 +10,82 @@ class SignUpPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Center(
-          child: Column(
-            children: [
-              const SizedBox(height: 100),
+        child: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              children: [
+                const SizedBox(height: 100),
 
-              // Sign up message
-              Text(
-                'Sign Up',
-                style: Theme.of(context).textTheme.displaySmall,
-              ),
+                // Sign up message
+                Text(
+                  'Sign Up',
+                  style: Theme.of(context).textTheme.displaySmall,
+                ),
 
-              const SizedBox(height: 50),
+                const SizedBox(height: 50),
 
-              // Name input
-              const InputTextField(
-                hintText: 'Full Name',
-              ),
+                // Name input
+                const InputTextField(
+                  hintText: 'Full Name',
+                ),
 
-              const SizedBox(height: 10),
+                const SizedBox(height: 10),
 
-              // Phone input
-              const InputTextField(hintText: 'Phone Number'),
+                // Phone input
+                const InputTextField(hintText: 'Phone Number'),
 
-              const SizedBox(height: 10),
+                const SizedBox(height: 10),
 
-              // Email input
-              const InputTextField(hintText: 'Email Address'),
+                // Email input
+                const InputTextField(hintText: 'Email Address'),
 
-              const SizedBox(height: 10),
+                const SizedBox(height: 10),
 
-              // Password input
-              const PasswordTextField(hintText: 'Password'),
+                // Password input
+                const PasswordTextField(hintText: 'Password'),
 
-              const SizedBox(height: 10),
+                const SizedBox(height: 10),
 
-              // Confirm password input
-              const PasswordTextField(hintText: 'Confirm Password'),
+                // Confirm password input
+                const PasswordTextField(hintText: 'Confirm Password'),
 
-              const SizedBox(height: 50),
+                const SizedBox(height: 50),
 
-              // Submit button
-              const SubmitButton(buttonText: 'Sign Up'),
+                // Submit button
+                const SubmitButton(buttonText: 'Sign Up'),
 
-              const SizedBox(height: 50),
+                const SizedBox(height: 50),
 
-              // Sign in link
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: RichText(
-                    text: TextSpan(
-                      children: [
-                        TextSpan(
-                            text: 'Already have an account? ',
+                // Sign in link
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: RichText(
+                      text: TextSpan(
+                        children: [
+                          TextSpan(
+                              text: 'Already have an account? ',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .titleSmall!
+                                  .copyWith(color: Colors.grey.shade700)),
+                          TextSpan(
+                            text: 'Sign In',
                             style: Theme.of(context)
                                 .textTheme
                                 .titleSmall!
-                                .copyWith(color: Colors.grey.shade700)),
-                        TextSpan(
-                          text: 'Sign In',
-                          style:
-                              Theme.of(context).textTheme.titleSmall!.copyWith(
-                                    color: Colors.orange,
-                                  ),
-                        ),
-                      ],
+                                .copyWith(
+                                  color: Colors.orange,
+                                ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
