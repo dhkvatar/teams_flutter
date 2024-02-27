d99413cf0913a8fa89dc3ca222f8ff115026b142
dhkatrv76 <dave.hongk@gmail.com>
2024-02-26 18:27:43 -0800

    firebase only in non-test mode

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -4,6 +4,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:teams/app/di/di.dart';
+import 'package:teams/core/configs/app_config.dart';
 import 'package:teams/core/flavors/flavors.dart';
 import 'package:teams/firebase_options_dev.dart' as fb_dev;
 import 'package:teams/firebase_options_prod.dart' as fb_prod;
@@ -17,16 +18,19 @@ Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
   // Set up app flavor.
   await Flavor.initializeAppFlavor();
 
-  // Set up Firebase.
-  switch (Flavor.appFlavor) {
-    case AppFlavor.dev:
-      await Firebase.initializeApp(
-          options: fb_dev.DefaultFirebaseOptions.currentPlatform);
-    case AppFlavor.prod:
-      await Firebase.initializeApp(
-          options: fb_prod.DefaultFirebaseOptions.currentPlatform);
-    case null:
-      break;
+  // Set up Firebase, unless in test mode.
+  // Fake repositories are injected in test mode.
+  if (!AppConfig.isTest) {
+    switch (Flavor.appFlavor) {
+      case AppFlavor.dev:
+        await Firebase.initializeApp(
+            options: fb_dev.DefaultFirebaseOptions.currentPlatform);
+      case AppFlavor.prod:
+        await Firebase.initializeApp(
+            options: fb_prod.DefaultFirebaseOptions.currentPlatform);
+      case null:
+        break;
+    }
   }
 
   // Configure dependency injections.
