a0d4a6df8292631b87016bbb1fc1dc3ffffd3480
dhkatrv76 <dave.hongk@gmail.com>
2024-02-27 02:10:39 -0800

    added tabs at top for dm and group chat

--- lib/presentation/pages/chat_page.dart
+++ lib/presentation/pages/chat_page.dart
@@ -5,9 +5,27 @@ class ChatPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('ChatPage'),
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: PreferredSize(
+          preferredSize: const Size.fromHeight(50.0),
+          child: AppBar(
+            bottom: const TabBar(
+              indicatorSize: TabBarIndicatorSize.label,
+              tabs: [
+                Tab(text: 'DM'),
+                Tab(text: 'Group Messages'),
+              ],
+            ),
+          ),
+        ),
+        body: const TabBarView(
+          children: [
+            Icon(Icons.cabin),
+            Icon(Icons.document_scanner),
+          ],
+        ),
       ),
     );
   }
