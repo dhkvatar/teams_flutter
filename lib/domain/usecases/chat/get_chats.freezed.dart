5f6c134b289d907e956eb3c1a004933e8f473092
dhkatrv76 <dave.hongk@gmail.com>
2024-02-28 08:38:15 -0800

    ChatBloc with GetChats usecase

---
+++ lib/domain/usecases/chat/get_chats.freezed.dart
@@ -0,0 +1,169 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'get_chats.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+/// @nodoc
+mixin _$GetChatsParams {
+  DateTime? get afterDateTime => throw _privateConstructorUsedError;
+  String? get afterId => throw _privateConstructorUsedError;
+  int? get limit => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $GetChatsParamsCopyWith<GetChatsParams> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $GetChatsParamsCopyWith<$Res> {
+  factory $GetChatsParamsCopyWith(
+          GetChatsParams value, $Res Function(GetChatsParams) then) =
+      _$GetChatsParamsCopyWithImpl<$Res, GetChatsParams>;
+  @useResult
+  $Res call({DateTime? afterDateTime, String? afterId, int? limit});
+}
+
+/// @nodoc
+class _$GetChatsParamsCopyWithImpl<$Res, $Val extends GetChatsParams>
+    implements $GetChatsParamsCopyWith<$Res> {
+  _$GetChatsParamsCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? afterDateTime = freezed,
+    Object? afterId = freezed,
+    Object? limit = freezed,
+  }) {
+    return _then(_value.copyWith(
+      afterDateTime: freezed == afterDateTime
+          ? _value.afterDateTime
+          : afterDateTime // ignore: cast_nullable_to_non_nullable
+              as DateTime?,
+      afterId: freezed == afterId
+          ? _value.afterId
+          : afterId // ignore: cast_nullable_to_non_nullable
+              as String?,
+      limit: freezed == limit
+          ? _value.limit
+          : limit // ignore: cast_nullable_to_non_nullable
+              as int?,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$GetChatsParamsImplCopyWith<$Res>
+    implements $GetChatsParamsCopyWith<$Res> {
+  factory _$$GetChatsParamsImplCopyWith(_$GetChatsParamsImpl value,
+          $Res Function(_$GetChatsParamsImpl) then) =
+      __$$GetChatsParamsImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call({DateTime? afterDateTime, String? afterId, int? limit});
+}
+
+/// @nodoc
+class __$$GetChatsParamsImplCopyWithImpl<$Res>
+    extends _$GetChatsParamsCopyWithImpl<$Res, _$GetChatsParamsImpl>
+    implements _$$GetChatsParamsImplCopyWith<$Res> {
+  __$$GetChatsParamsImplCopyWithImpl(
+      _$GetChatsParamsImpl _value, $Res Function(_$GetChatsParamsImpl) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? afterDateTime = freezed,
+    Object? afterId = freezed,
+    Object? limit = freezed,
+  }) {
+    return _then(_$GetChatsParamsImpl(
+      afterDateTime: freezed == afterDateTime
+          ? _value.afterDateTime
+          : afterDateTime // ignore: cast_nullable_to_non_nullable
+              as DateTime?,
+      afterId: freezed == afterId
+          ? _value.afterId
+          : afterId // ignore: cast_nullable_to_non_nullable
+              as String?,
+      limit: freezed == limit
+          ? _value.limit
+          : limit // ignore: cast_nullable_to_non_nullable
+              as int?,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$GetChatsParamsImpl implements _GetChatsParams {
+  const _$GetChatsParamsImpl({this.afterDateTime, this.afterId, this.limit});
+
+  @override
+  final DateTime? afterDateTime;
+  @override
+  final String? afterId;
+  @override
+  final int? limit;
+
+  @override
+  String toString() {
+    return 'GetChatsParams(afterDateTime: $afterDateTime, afterId: $afterId, limit: $limit)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$GetChatsParamsImpl &&
+            (identical(other.afterDateTime, afterDateTime) ||
+                other.afterDateTime == afterDateTime) &&
+            (identical(other.afterId, afterId) || other.afterId == afterId) &&
+            (identical(other.limit, limit) || other.limit == limit));
+  }
+
+  @override
+  int get hashCode => Object.hash(runtimeType, afterDateTime, afterId, limit);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$GetChatsParamsImplCopyWith<_$GetChatsParamsImpl> get copyWith =>
+      __$$GetChatsParamsImplCopyWithImpl<_$GetChatsParamsImpl>(
+          this, _$identity);
+}
+
+abstract class _GetChatsParams implements GetChatsParams {
+  const factory _GetChatsParams(
+      {final DateTime? afterDateTime,
+      final String? afterId,
+      final int? limit}) = _$GetChatsParamsImpl;
+
+  @override
+  DateTime? get afterDateTime;
+  @override
+  String? get afterId;
+  @override
+  int? get limit;
+  @override
+  @JsonKey(ignore: true)
+  _$$GetChatsParamsImplCopyWith<_$GetChatsParamsImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
---
+++ lib/presentation/blocs/chat/chat_state.dart
@@ -0,0 +1,37 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:teams/domain/entities/chat.dart';
+import 'package:teams/domain/entities/message.dart';
+
+part 'chat_state.freezed.dart';
+
+@freezed
+class ChatState with _$ChatState {
+  const factory ChatState({
+    // List of direct message chats, sorted by updateTime and Id (descending).
+    @Default([]) List<Chat> directMessageChats,
+
+    // List of group chats, sorted by updateTime and Id (descending).
+    @Default([]) List<Chat> groupChats,
+
+    // The last direct message chat by updateTime and Id, i.e., the earliest
+    // created with the smallest Id in directMessageChats.
+    Chat? lastDirectMessageChat,
+
+    // The last group chat by updateTime and Id, i.e., the earliest created
+    // with the smallest Id in groupChats.
+    Chat? lastGroupChat,
+
+    // Map from Chat Id to the last message (earliest timestamp and smallest
+    // Id) for each chat.
+    Map<String, Message>? lastMessage,
+    @Default(ChatsLoadingStatus.complete) chatsLoadingStatus,
+
+    // Error message to display after processing a ChatEvent.
+    String? errorMessage,
+  }) = _ChatState;
+}
+
+enum ChatsLoadingStatus {
+  inProgress,
+  complete,
+}
