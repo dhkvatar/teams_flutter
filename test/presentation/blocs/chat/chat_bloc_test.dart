50d4147a65f921ce0ffe1cd772d3c54cec037cdd
dhkatrv76 <dave.hongk@gmail.com>
2024-03-04 13:28:52 -0800

    breakpoint

--- test/presentation/blocs/chat/chat_bloc_test.dart
+++ test/presentation/blocs/chat/chat_bloc_test.dart
@@ -1,11 +1,13 @@
 import 'dart:async';
 
+// import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:teams/domain/repositories/chat_repository.dart';
+// import 'package:teams/domain/usecases/chat/get_chat_updates_stream.dart';
 import 'package:teams/presentation/blocs/chat/chat_bloc.dart';
-import 'package:teams/presentation/blocs/chat/chat_event.dart';
+// import 'package:teams/presentation/blocs/chat/chat_event.dart';
 import 'package:teams/presentation/blocs/chat/chat_state.dart';
 
 import 'chat_bloc_test.mocks.dart';
@@ -27,6 +29,26 @@ void main() {
       });
     });
 
-    group(ChatGetChatsRequested, () {});
+    //   group(ChatGetChatsRequested, () {
+    //     late MockStream<ChatUpdateStreamItem> mockStream;
+    //     blocTest(
+    //       'emits ChatsLoadingStatus.inProgress first',
+    //       setUp: () {
+    //         mockStream = MockStream<ChatUpdateStreamItem>();
+    //         when(mockStream.listen(any)).thenAnswer(
+    //           (inv) =>
+    //               const Stream<ChatUpdateStreamItem>.empty().listen((event) {}),
+    //         );
+    //       },
+    //       build: () => ChatBloc.fromStream(stream: mockStream),
+    //       act: (bloc) => bloc.add(const ChatGetChatsRequested()),
+    //       expect: () => [
+    //         const ChatState(),
+    //         const ChatState()
+    //             .copyWith(chatsLoadingStatus: ChatsLoadingStatus.inProgress),
+    //       ],
+    //     );
+    //   });
+    // });
   });
 }
